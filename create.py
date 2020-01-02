97f1b6ddef46dbb2473a02472c51a7da61c346b6
danielmestrez <41844929+danielmestrez@users.noreply.github.com>
2020-01-02 04:03:12 +0000

    Initial version 0.1
    the most basic functionality.
    
    Reads a word file in the format:
    1 de [month]
    https://link1.com
    Lorem Ipsum ...
    asfcasfasdcf
    asfasdf
    
    https://link2.com
    Lorem Ipsum ...
    asfcasfasdcf
    asfasdf
    
    2 de [month]
    https://link3.com
    Lorem Ipsum ...
    asfcasfasdcf
    asfasdf
    
    And generates a report from that

---
+++ create.py
@@ -0,0 +1,77 @@
+# -*- coding: cp1252 -*-
+from docx import Document
+import read_docx
+import re
+from datetime import datetime
+import sys
+droppedFile = sys.argv[1]
+print droppedFile
+
+months = (
+        'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+        'September', 'October', 'November', 'December', 'janeiro', 'fevereiro', 'março',
+        'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro',
+        'dezembro'
+         )
+
+document = Document()
+
+# Import do documento, deverá aparecer neste formato
+
+records = []
+total_letters = 0
+docx_text = read_docx.get_docx_text('IMPORT.docx').splitlines()
+
+total_letters = 0
+date = ""
+link = ""
+letter_count = 0
+
+def addEntry():
+    if link != "":
+        global date
+        global link
+        global letter_count
+        global total_letters
+        total_letters += letter_count
+        records.append((date, link, letter_count))
+        date = ""
+        link = ""
+        letter_count = 0
+
+
+for line in docx_text:
+    if line == "":
+        continue
+    
+    date_match = re.search(r'^\d de \w', line)
+    link_match = re.search(r'^http', line)
+
+    if date_match:
+        addEntry()
+        date = line
+    elif link_match:
+        addEntry()
+        link = line
+    else:
+        letter_count += len(line)
+                
+#Apartir dos resultados lidos do ficheiro grande, gerar a tabela
+p = document.add_paragraph('Total letras: ' + str(total_letters))
+table = document.add_table(rows=1, cols=3)
+table.allow_autofit = True
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Data'
+hdr_cells[1].text = 'Link'
+hdr_cells[2].text = 'Carateres'
+for date, link, letter_count in records:
+    row_cells = table.add_row().cells
+    row_cells[0].text = date
+    row_cells[1].text = link
+    row_cells[2].text = str(letter_count)
+
+time = str(datetime.now().strftime('%Y-%m-%d-%H%M'))
+document.save('Relatório-' + time + '.docx')
+
+    
+
